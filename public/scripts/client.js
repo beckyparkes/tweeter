556b90ae3230f5362c47b854963617b92eb985ad
Rebekah <rebekahmparkes@outlook.com>
2021-08-12 20:26:17 -0400

    right order with data

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -6,83 +6,113 @@
 
 
 $(document).ready(function() {
-  
 
 
-// function takes in a tweet object and is responsible for returning a tweet <article> element containing the entire HTML structure of the tweet.takes in a tweet object and is responsible for returning a tweet <article> element containing takes in a tweet object and is responsible for returning a tweet <article> element containing 
-// the entire HTML structure of the tweet.the entire HTML structure of the tweet.
-const createTweetElement = function(tweetObject) { 
-
-//   const tweetData = {
-//     "user": {
-//       "name": "Newton",
-//       "avatars": "https://i.imgur.com/73hZDYK.png",
-//         "handle": "@SirIsaac"
-//       },
-//     "content": {
-//         "text": "If I have seen further it is by standing on the shoulders of giants"
-//       },
-//     "created_at": 1461116232227
-//  }
-
-// const $tweet = createTweetElement(tweetData);
-
-// // Test / driver code (temporary)
-// console.log($tweet); // to see what it looks like
-// $('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
-
-
-
-// const $tweet = $(`<article class="tweet">Hello world</article>`);
-  
-  // const markup =
-
-  // `   <article class='container'>
-  //    <header id="article-header"><i style="float:left; margin: 0.2em" class="fas fa-smile"></i>&nbsp&nbsp ${user.name}</header>
-  //    <footer id="article-footer" class="container">&nbsp ${user[created_at]}
-  //      <div id="footer-emotes">
-  //      <i class="fas fa-flag"></i>&nbsp<i class="fas fa-retweet"></i>&nbsp<i class="fas fa-heart"></i>
-  //      </div>
-  //    </footer>
-  //  </article>`;
-
-
-  function createTweetElement(tweetData) {
-    let tweet = $('<article>').addClass('tweet');
-    
-    const header = $('<header>');
-    const img = $('<img>', {
-      'src': tweetData.user.avatars.small 
-    });
-    const h2 = $('<h2>', {
-      text: tweetData.user.name
-    });
-    const handle = $('<p>', {
-      'class': 'handle',
-      text: tweetData.user.handle
-    });
-    const p = $('<p>', {
-      text: tweetData.content.text
-    });
-    const footer = $("<footer>", {
-      text: Math.round(((Date.now() - tweetData.created_at) / 86400000))  + ' days ago'
-    });
-    
-    header.append(img, h2, handle);
-    tweet.append(header, p, footer);
-    
-    return tweet;
+  // Fake data taken from initial-tweets.json
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
   }
+]
 
-  }
 
-  function renderTweets(tweetData) {
+  function renderTweets(tweets) {
+  // loops through tweets
+  // calls createTweetElement for each tweet
+  // takes return value and appends it to the tweets container
     tweetData.forEach((item) => {
       return createTweetElement(item).appendTo('.tweets');
     })
   }
 
+    
+
+
+// function takes in a tweet object and is responsible for returning a tweet <article> element containing the entire HTML structure of the tweet.takes in a tweet object and is responsible for returning a tweet <article> element containing takes in a tweet object and is responsible for returning a tweet <article> element containing 
+// the entire HTML structure of the tweet.the entire HTML structure of the tweet.
+const createTweetElement = function(tweet) { 
+
+  //   const tweetData = {
+  //     "user": {
+  //       "name": "Newton",
+  //       "avatars": "https://i.imgur.com/73hZDYK.png",
+  //         "handle": "@SirIsaac"
+  //       },
+  //     "content": {
+  //         "text": "If I have seen further it is by standing on the shoulders of giants"
+  //       },
+  //     "created_at": 1461116232227
+  //  }
   
+  // const $tweet = createTweetElement(tweetData);
+  
+  // // Test / driver code (temporary)
+  // console.log($tweet); // to see what it looks like
+  // $('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+  
+  
+  
+  // const $tweet = $(`<article class="tweet">Hello world</article>`);
+    
+    // const markup =
+  
+    // `   <article class='container'>
+    //    <header id="article-header"><i style="float:left; margin: 0.2em" class="fas fa-smile"></i>&nbsp&nbsp ${user.name}</header>
+    //    <footer id="article-footer" class="container">&nbsp ${user[created_at]}
+    //      <div id="footer-emotes">
+    //      <i class="fas fa-flag"></i>&nbsp<i class="fas fa-retweet"></i>&nbsp<i class="fas fa-heart"></i>
+    //      </div>
+    //    </footer>
+    //  </article>`;
+  
+  
+  
+      let tweet = $('<article>').addClass('tweet');
+      
+      const header = $('<header>');
+      const img = $('<img>', {
+        'src': tweetData.user.avatars.small 
+      });
+      const h2 = $('<h2>', {
+        text: tweetData.user.name
+      });
+      const handle = $('<p>', {
+        'class': 'handle',
+        text: tweetData.user.handle
+      });
+      const p = $('<p>', {
+        text: tweetData.content.text
+      });
+      const footer = $("<footer>", {
+        text: Math.round(((Date.now() - tweetData.created_at) / 86400000))  + ' days ago'
+      });
+      
+      header.append(img, h2, handle);
+      tweet.append(header, p, footer);
+      
+      return tweet;
+    }
+
+    renderTweets(data);
 });
 
 
