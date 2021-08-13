71ac06b959d58fadfb0f0a5705f0981481c0b2a2
Rebekah <rebekahmparkes@outlook.com>
2021-08-13 14:06:26 -0400

    dynamic allocation working

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -7,40 +7,38 @@
 
 $(document).ready(function() {
 
-  // function loadTweets() {
-  //   $.ajax({
-  //     type: 'GET',
-  //     url: '/tweets',
-  //     success: function(data) {
-  //       renderTweets(data);
-  //     }
-  //   });
-  // }
+  function loadTweets() {
+    $.ajax({
+      type: 'GET',
+      url: '/tweets',
+      success: function(data) {
+        renderTweets(data);
+      }
+    });
+  }
 
+  // action when submitting
+  $("#make-new-tweet").on('submit', (event) => {
+    event.preventDefault();
 
+    let data = ($(event.target).serialize()); //taking data from the input field and turning it into a query string
+    let inputText = $(event.target).children('textarea').val();
 
-  //action when submitting
-  // $("#new-tweet").on('submit', (event) => {
-  //   event.preventDefault();
-
-  //   let data = ($(event.target).serialize()); //taking data from the input field and turning it into a query string
-  //   let inputText = $(event.target).children('textarea').val();
-
-  //   if (inputText === '') {
-  //     $('.error').text("Please enter a tweet before submitting!").hide();
-  //     $('.error').slideDown("slow");
-  //   } else if (inputText.length > 140) {
-  //     $('.error').text("Too many characters!").hide();
-  //     $('.error').slideDown();
-  //   } else {
-  //     $.ajax('/tweets', {method: 'POST', data: data}).then(() => { //using ajax to make a post request and render the new tweets without reloading the page
-  //       $(event.target).children('textarea').val(''); //clears text area
-  //       $('.counter').text(140); //reset text area
-  //       $('.error').hide();
-  //       loadTweets();
-  //     });
-  //   }
-  // });
+    if (inputText === '') {
+      $('.error').text("Please enter a tweet before submitting!").hide();
+      $('.error').slideDown("slow");
+    } else if (inputText.length > 140) {
+      $('.error').text("Too many characters!").hide();
+      $('.error').slideDown();
+    } else {
+      $.ajax('/tweets', {method: 'POST', data: data}).then(() => { //using ajax to make a post request and render the new tweets without reloading the page
+        $(event.target).children('textarea').val(''); //clears text area
+        $('.counter').text(140); //reset text area
+        $('.error').hide();
+        loadTweets();
+      });
+    }
+  });
 
 
   // // disallow when certain conditions aren't met
@@ -72,44 +70,13 @@ $(document).ready(function() {
 
 
 
-
-// function takes in a tweet object and is responsible for returning a tweet <article> element containing the entire HTML structure of the tweet.takes in a tweet object and is responsible for returning a tweet <article> element containing takes in a tweet object and is responsible for returning a tweet <article> element containing 
-// the entire HTML structure of the tweet.the entire HTML structure of the tweet.
-
-
-  // takes in this object "tweet"
-  const tweetData = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png"
-        ,
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd" },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ]
-
 const renderTweets = function(tweets) {
     // loops through tweets
     // calls createTweetElement for each tweet
     // takes return value and appends it to the tweets container
   
       tweets.forEach((item) => {
-        return createTweetElement(item).appendTo('#tweets-container');
+        return createTweetElement(item).prependTo('#tweets-container');
       })
     }
 
@@ -134,8 +101,6 @@ const createTweetElement = function(data) {
   
   return $newTweet;
 }
-
-  renderTweets(tweetData);
 
 
 });
