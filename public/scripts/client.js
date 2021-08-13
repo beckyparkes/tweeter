907da0fe98605c1ba798bcdb88fdce5c71b7f1a7
Rebekah <rebekahmparkes@outlook.com>
2021-08-13 08:46:46 -0400

    test drive code FINALLY works omg

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -7,68 +7,68 @@
 
 $(document).ready(function() {
 
-  function loadTweets() {
-    $.ajax({
-      type: 'GET',
-      url: '/tweets',
-      success: function(data) {
-        renderTweets(data);
-      }
-    });
-  }
+  // function loadTweets() {
+  //   $.ajax({
+  //     type: 'GET',
+  //     url: '/tweets',
+  //     success: function(data) {
+  //       renderTweets(data);
+  //     }
+  //   });
+  // }
 
 
 
   //action when submitting
-  $("#new-tweet").on('submit', (event) => {
-    event.preventDefault();
+  // $("#new-tweet").on('submit', (event) => {
+  //   event.preventDefault();
 
-    let data = ($(event.target).serialize()); //taking data from the input field and turning it into a query string
-    let inputText = $(event.target).children('textarea').val();
+  //   let data = ($(event.target).serialize()); //taking data from the input field and turning it into a query string
+  //   let inputText = $(event.target).children('textarea').val();
 
-    if (inputText === '') {
-      $('.error').text("Please enter a tweet before submitting!").hide();
-      $('.error').slideDown("slow");
-    } else if (inputText.length > 140) {
-      $('.error').text("Too many characters!").hide();
-      $('.error').slideDown();
-    } else {
-      $.ajax('/tweets', {method: 'POST', data: data}).then(() => { //using ajax to make a post request and render the new tweets without reloading the page
-        $(event.target).children('textarea').val(''); //clears text area
-        $('.counter').text(140); //reset text area
-        $('.error').hide();
-        loadTweets();
-      });
-    }
-  });
+  //   if (inputText === '') {
+  //     $('.error').text("Please enter a tweet before submitting!").hide();
+  //     $('.error').slideDown("slow");
+  //   } else if (inputText.length > 140) {
+  //     $('.error').text("Too many characters!").hide();
+  //     $('.error').slideDown();
+  //   } else {
+  //     $.ajax('/tweets', {method: 'POST', data: data}).then(() => { //using ajax to make a post request and render the new tweets without reloading the page
+  //       $(event.target).children('textarea').val(''); //clears text area
+  //       $('.counter').text(140); //reset text area
+  //       $('.error').hide();
+  //       loadTweets();
+  //     });
+  //   }
+  // });
 
 
-  // disallow when certain conditions aren't met
+  // // disallow when certain conditions aren't met
 
-  $('.new-tweet form').submit((event) => {
-    event.preventDefault();
-    const charLeft = Number($('.counter').text());
-    if (charLeft == 140) {
-      $('.popup-msg').text('Write something before submitting.').slideDown(function() {
-        setTimeout(function() {
-          $('.popup-msg').slideUp();
-        }, 5000);
-      });
-    } else if (charLeft < 0) {
-      $('.popup-msg').text('Oops, you wrote too much!').slideDown(function() {
-        setTimeout(function() {
-          $('.popup-msg').slideUp();
-        }, 5000);
-      });
-    } else {
-      $.ajax({
-        type: 'POST',
-        url: '/tweets',
-        data: $('.new-tweet form textarea').serialize(),
-        success: renderNewTweet
-      });
-    }
-  });
+  // $('.new-tweet form').submit((event) => {
+  //   event.preventDefault();
+  //   const charLeft = Number($('.counter').text());
+  //   if (charLeft == 140) {
+  //     $('.popup-msg').text('Write something before submitting.').slideDown(function() {
+  //       setTimeout(function() {
+  //         $('.popup-msg').slideUp();
+  //       }, 5000);
+  //     });
+  //   } else if (charLeft < 0) {
+  //     $('.popup-msg').text('Oops, you wrote too much!').slideDown(function() {
+  //       setTimeout(function() {
+  //         $('.popup-msg').slideUp();
+  //       }, 5000);
+  //     });
+  //   } else {
+  //     $.ajax({
+  //       type: 'POST',
+  //       url: '/tweets',
+  //       data: $('.new-tweet form textarea').serialize(),
+  //       success: renderNewTweet
+  //     });
+  //   }
+  // });
   
 
 // ----------------   Fake data taken from initial-tweets.json ------------------------
@@ -98,61 +98,59 @@ $(document).ready(function() {
 // ]
 
 
-  function renderTweets(tweets) {
-  // loops through tweets
-  // calls createTweetElement for each tweet
-  // takes return value and appends it to the tweets container
+  // function renderTweets(tweets) {
+  // // loops through tweets
+  // // calls createTweetElement for each tweet
+  // // takes return value and appends it to the tweets container
 
-    tweets.forEach((item) => {
-      return createTweetElement(item).appendTo('.tweets');
-    })
-  }
-
-    
+  //   tweets.forEach((item) => {
+  //     return createTweetElement(item).appendTo('#tweets-container');
+  //   })
+  // }
 
 
 // function takes in a tweet object and is responsible for returning a tweet <article> element containing the entire HTML structure of the tweet.takes in a tweet object and is responsible for returning a tweet <article> element containing takes in a tweet object and is responsible for returning a tweet <article> element containing 
 // the entire HTML structure of the tweet.the entire HTML structure of the tweet.
 
-const createTweetElement = function(tweet) { 
+
   // takes in this object "tweet"
-//   {
-//     "user": {
-//       "name": "Newton",
-//       "avatars": "https://i.imgur.com/73hZDYK.png",
-//         "handle": "@SirIsaac"
-//       },
-//     "content": {
-//         "text": "If I have seen further it is by standing on the shoulders of giants"
-//       },
-//     "created_at": 1461116232227
-//  }
+  const tweetData = {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png",
+        "handle": "@SirIsaac"
+      },
+    "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+    "created_at": 1461116232227
+  }
 
 
-      let tweet = $('<article>').addClass('tweet');
-      const header = $('<header>');
-      const h2 = $('<h2>', {
-        text: tweetData.user.name
-      });
-      const handle = $('<p>', {
-        'class': 'handle',
-        text: tweetData.user.handle
-      });
-      const p = $('<p>', {
-        text: tweetData.content.text
-      });
-      const footer = $("<footer>", {
+const createTweetElement = function(data) { 
+  const newTweet = 
+  $(`<article class='container'>
 
-        // !!!!!!!! Want to be using timeago, get help
-      });
-      
-      header.append(h2, handle);
-      tweet.append(header, p, footer);
-      
-      return tweet;
-    }
+    <header id="article-header"><i style="float:left; margin: 0.2em" class="fas fa-smile"></i>
+      <div id="name">&nbsp&nbsp ${data.user.name}</div>
+      <div id="handle">${data.user.handle}</div>
+    </header>
 
-    renderTweets(data);
+    <div id="article-body">${data.content.text}</div>
+    <footer id="article-footer" class="container">&nbsp ${data.created_at}
+      <div id="footer-emotes">
+      <i class="fas fa-flag"></i>&nbsp<i class="fas fa-retweet"></i>&nbsp<i class="fas fa-heart"></i>
+      </div>
+    </footer>
+  </article>`);
+  
+  return newTweet;
+}
+
+const $tweet = createTweetElement(tweetData);
+console.log($tweet);
+$('#tweets-container').append($tweet);
+
 });
 
 
