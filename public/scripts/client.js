532f3b629d38e2974370003ac80412aa8052c6a6
Rebekah <rebekahmparkes@outlook.com>
2021-08-13 09:01:51 -0400

    multiple tweets works

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -98,15 +98,7 @@ $(document).ready(function() {
 // ]
 
 
-  // function renderTweets(tweets) {
-  // // loops through tweets
-  // // calls createTweetElement for each tweet
-  // // takes return value and appends it to the tweets container
 
-  //   tweets.forEach((item) => {
-  //     return createTweetElement(item).appendTo('#tweets-container');
-  //   })
-  // }
 
 
 // function takes in a tweet object and is responsible for returning a tweet <article> element containing the entire HTML structure of the tweet.takes in a tweet object and is responsible for returning a tweet <article> element containing takes in a tweet object and is responsible for returning a tweet <article> element containing 
@@ -114,22 +106,45 @@ $(document).ready(function() {
 
 
   // takes in this object "tweet"
-  const tweetData = {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png",
+  const tweetData = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png"
+        ,
         "handle": "@SirIsaac"
       },
-    "content": {
+      "content": {
         "text": "If I have seen further it is by standing on the shoulders of giants"
       },
-    "created_at": 1461116232227
-  }
+      "created_at": 1461116232227
+    },
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd" },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ]
+
+const renderTweets = function(tweets) {
+    // loops through tweets
+    // calls createTweetElement for each tweet
+    // takes return value and appends it to the tweets container
+  
+      tweets.forEach((item) => {
+        return createTweetElement(item).appendTo('#tweets-container');
+      })
+    }
 
 
 const createTweetElement = function(data) {
   const timeAgo = timeago.format(data.created_at);
-  const newTweet = 
+  let $newTweet = 
   $(`<article class='container'>
 
     <header id="article-header"><i style="float:left; margin: 0.2em" class="fas fa-smile"></i>
@@ -145,12 +160,11 @@ const createTweetElement = function(data) {
     </footer>
   </article>`);
   
-  return newTweet;
+  return $newTweet;
 }
 
-const $tweet = createTweetElement(tweetData);
-console.log($tweet);
-$('#tweets-container').append($tweet);
+  renderTweets(tweetData);
+
 
 });
 
