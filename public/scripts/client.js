b4561f98630329452023b36588e3eae4ed54d092
Rebekah <rebekahmparkes@outlook.com>
2021-08-13 06:07:14 -0400

    client updates

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -115,6 +115,7 @@ $(document).ready(function() {
 
 // function takes in a tweet object and is responsible for returning a tweet <article> element containing the entire HTML structure of the tweet.takes in a tweet object and is responsible for returning a tweet <article> element containing takes in a tweet object and is responsible for returning a tweet <article> element containing 
 // the entire HTML structure of the tweet.the entire HTML structure of the tweet.
+
 const createTweetElement = function(tweet) { 
       let tweet = $('<article>').addClass('tweet');
       const header = $('<header>');
@@ -143,96 +144,3 @@ const createTweetElement = function(tweet) {
 });
 
 
-
-
-// $(document).ready(function() {
-  
-//   function loadTweets() {
-//     $.ajax({
-//       type: 'GET',
-//       url: '/tweets',
-//       success: function(data) {
-//         renderTweets(data);
-//       }
-//     });
-//   }
-  
-//   loadTweets();
-  
-//   $('.new-tweet form').submit((event) => {
-//     event.preventDefault();
-//     const charLeft = Number($('.counter').text());
-//     if (charLeft == 140) {
-//       $('.popup-msg').text('Write something before submitting.').slideDown(function() {
-//         setTimeout(function() {
-//           $('.popup-msg').slideUp();
-//         }, 5000);
-//       });
-//     } else if (charLeft < 0) {
-//       $('.popup-msg').text('Oops, you wrote too much!').slideDown(function() {
-//         setTimeout(function() {
-//           $('.popup-msg').slideUp();
-//         }, 5000);
-//       });
-//     } else {
-//       $.ajax({
-//         type: 'POST',
-//         url: '/tweets',
-//         data: $('.new-tweet form textarea').serialize(),
-//         success: renderNewTweet
-//       });
-//     }
-//   });
-  
-//   function renderNewTweet() {
-//     $.ajax({
-//       type: 'GET',
-//       url: '/tweets',
-//       success: function(data) {
-//         const newTweet = createTweetElement(data[0]);
-//         newTweet.prependTo('.tweets');
-//       }
-//     });
-//   }
-  
-//   function renderTweets(tweetData) {
-//     tweetData.forEach((item) => {
-//       return createTweetElement(item).appendTo('.tweets');
-//     })
-//   }
-  
-//   function createTweetElement(tweetData) {
-//     let tweet = $('<article>').addClass('tweet');
-    
-//     const header = $('<header>');
-//     const img = $('<img>', {
-//       'src': tweetData.user.avatars.small 
-//     });
-//     const h2 = $('<h2>', {
-//       text: tweetData.user.name
-//     });
-//     const handle = $('<p>', {
-//       'class': 'handle',
-//       text: tweetData.user.handle
-//     });
-//     const p = $('<p>', {
-//       text: tweetData.content.text
-//     });
-//     const footer = $("<footer>", {
-//       text: Math.round(((Date.now() - tweetData.created_at) / 86400000))  + ' days ago'
-//     });
-    
-//     header.append(img, h2, handle);
-//     tweet.append(header, p, footer);
-    
-//     return tweet;
-//   }
-
-//   // Compose button behavior
-//   $('nav .compose').click((event) => {
-//     event.preventDefault();
-//     $('.new-tweet').slideToggle();
-//     $('.new-tweet textarea').focus();
-//   })
-  
-// });
