2095dacedbe58ba13945e7915c16426014db64b0
Rebekah <rebekahmparkes@outlook.com>
2021-08-13 14:56:11 -0400

    img on CreateTweet working

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -5,13 +5,13 @@
  */
 
 
-$(document).ready(function() {
+$(document).ready(function () {
 
   function loadTweets() {
     $.ajax({
       type: 'GET',
       url: '/tweets',
-      success: function(data) {
+      success: function (data) {
         renderTweets(data);
       }
     });
@@ -24,14 +24,15 @@ $(document).ready(function() {
     let data = ($(event.target).serialize()); //taking data from the input field and turning it into a query string
     let inputText = $(event.target).children('textarea').val();
 
-    if (inputText === '') {
+    if (inputText === '' || inputText === null) {
       $('.error').text("Please enter a tweet before submitting!").hide();
       $('.error').slideDown("slow");
     } else if (inputText.length > 140) {
       $('.error').text("Too many characters!").hide();
       $('.error').slideDown();
+
     } else {
-      $.ajax('/tweets', {method: 'POST', data: data}).then(() => { //using ajax to make a post request and render the new tweets without reloading the page
+      $.ajax('/tweets', { method: 'POST', data: data }).then(() => { //using ajax to make a post request and render the new tweets without reloading the page
         $(event.target).children('textarea').val(''); //clears text area
         $('.counter').text(140); //reset text area
         $('.error').hide();
@@ -40,68 +41,36 @@ $(document).ready(function() {
     }
   });
 
-
-  // disallow when certain conditions aren't met
-
-  $("#make-new-tweet").submit((event) => {
-    event.preventDefault();
-    const chars = Number($('.counter').text());
-    if (chars === 140) {
-      $('.popup-msg').text('Form cannot be empty.').slideDown(function() {
-        setTimeout(function() {
-          $('.popup-msg').slideUp();
-        }, 5000);
-      });
-    } else if (chars < 0) {
-      $('.popup-msg').text("Message must remain under 140 characters").slideDown(function() {
-        setTimeout(function() {
-          $('.popup-msg').slideUp();
-        }, 5000);
-      });
-    } else {
-      $.ajax({
-        type: 'POST',
-        url: '/tweets',
-        data: $('.new-tweet form textarea').serialize(),
-        success: renderNewTweet
-      });
-    }
-  });
+//loops through list and appends to text area
+  const renderTweets = function (tweets) {
+    tweets.forEach((item) => {
+      return createTweetElement(item).prependTo('#tweets-container');
+    })
+  }
 
 
+  const createTweetElement = function (data) {
+    const timeAgo = timeago.format(data.created_at);
+    let $newTweet =
+      $(`<article class='container'>
 
-const renderTweets = function(tweets) {
-    // loops through tweets
-    // calls createTweetElement for each tweet
-    // takes return value and appends it to the tweets container
-  
-      tweets.forEach((item) => {
-        return createTweetElement(item).prependTo('#tweets-container');
-      })
-    }
+        <header id="article-header"><img src="${data.user.avatars}" style="float:left; margin: 0.2em" class="fas fa-smile"></i>
+          <div id="name">&nbsp&nbsp ${data.user.name}</div>
+          <div id="handle">${data.user.handle}</div>
+        </header>
 
+        <div id="article-body">${data.content.text}</div>
+        <footer id="article-footer" class="container">&nbsp ${timeAgo}
+          <div id="footer-emotes">
+          <i class="fas fa-flag"></i>&nbsp<i class="fas fa-retweet"></i>&nbsp<i class="fas fa-heart"></i>
+          </div>
+        </footer>
+      </article>`);
 
-const createTweetElement = function(data) {
-  const timeAgo = timeago.format(data.created_at);
-  let $newTweet = 
-  $(`<article class='container'>
+    return $newTweet;
+  }
 
-    <header id="article-header"><i style="float:left; margin: 0.2em" class="fas fa-smile"></i>
-      <div id="name">&nbsp&nbsp ${data.user.name}</div>
-      <div id="handle">${data.user.handle}</div>
-    </header>
-
-    <div id="article-body">${data.content.text}</div>
-    <footer id="article-footer" class="container">&nbsp ${timeAgo}
-      <div id="footer-emotes">
-      <i class="fas fa-flag"></i>&nbsp<i class="fas fa-retweet"></i>&nbsp<i class="fas fa-heart"></i>
-      </div>
-    </footer>
-  </article>`);
-  
-  return $newTweet;
-}
-
+  // <header id="article-header"><img src="${data.user.avatars}" style="float:left; margin: 0.2em" class="fas fa-smile"></i>
 
 });
 
